2210d5d46b42fd7b1874bd0cfe6ee0f90f0b3498
TuomasJalonen <jalonen3@wks-92749-mac.ad.tuni.fi>
2020-07-27 19:21:04 +0300

    1.11*: unicafe step6

--- Part1/unicafe/src/index.js
+++ Part1/unicafe/src/index.js
@@ -15,7 +15,10 @@ const Button = (props) => (
 
 const StatisticLine = (props) => {
   return (
-    <p>{props.text} {props.value}</p>
+    <tr>
+      <td>{props.text} </td>
+      <td>{props.value} </td>
+    </tr>
   )
 }
 
@@ -30,12 +33,16 @@ const Statistics = (props) => {
 
   return (
     <div>
-      <StatisticLine text="good" value={props.good} />
-      <StatisticLine text="neutral" value={props.neutral} />
-      <StatisticLine text="bad" value={props.bad} />
-      <StatisticLine text="all" value={all} />
-      <StatisticLine text="average" value={(props.good - props.bad) / all} />
-      <StatisticLine text="positive" value={(props.good / all) * 100 + "%"} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={props.good} />
+          <StatisticLine text="neutral" value={props.neutral} />
+          <StatisticLine text="bad" value={props.bad} />
+          <StatisticLine text="all" value={all} />
+          <StatisticLine text="average" value={(props.good - props.bad) / all} />
+          <StatisticLine text="positive" value={(props.good / all) * 100 + " %"} />
+        </tbody>
+      </table>
     </div>
   )
 }
