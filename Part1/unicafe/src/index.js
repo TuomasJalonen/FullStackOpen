53255c5d0539a4a51a2000323edbad2c6cdd8808
TuomasJalonen <jalonen3@wks-92749-mac.ad.tuni.fi>
2020-07-27 17:32:59 +0300

    1.6: unicafe step1

---
+++ Part1/unicafe/src/index.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Display = props => <div>{props.value}</div>
+
+
+const App = props => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const setToGood = newValue => {
+    setGood(newValue)
+  }
+  const setToNeutral = newValue => {
+    setNeutral(newValue)
+  }
+  const setToBad = newValue => {
+    setBad(newValue)
+  }
+
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setToGood(good + 1)} text="good" />
+      <Button handleClick={() => setToNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setToBad(bad + 1)} text="bad" />
+      <h1>statistics</h1>
+      <p>good {good} </p>
+      <p>neutral {neutral} </p>
+      <p>bad {bad} </p>
+ 
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
